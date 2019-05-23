1a2e20748f49005a0ba25de58db45d34a07cf897
Efrain Vergara <akai10tsuki@gmail.com>
2019-05-23 00:35:40 -0400

    chg: refactoring

--- vsutillib/classes/runcommand.py
+++ vsutillib/classes/runcommand.py
@@ -32,79 +32,82 @@ class RunCommand:
     """
 
     def __init__(
-        self,
-        command=None,
-        processLine=None,
-        processArgs=None,
-        processKWArgs=None,
-        regexsearch=None,
-        commandShlex=False,
-        universalNewLines=False
-    ):
+            self,
+            command=None,
+            processLine=None,
+            processArgs=None,
+            processKWArgs=None,
+            regexsearch=None,
+            commandShlex=False,
+            universalNewLines=False
+        ):
 
-        self._command = command
-        self._process = processLine
-        self._commandShlex = commandShlex
-        self._universalNewLines = universalNewLines
-        self._processArgs = []
+        self.__command = None
+        self.command = command
+
+        self.__commandShlex = commandShlex
+        self.__process = processLine
+        self.__universalNewLines = universalNewLines
+        self.__processArgs = []
 
         if processArgs is not None:
             if isinstance(processArgs, list):
-                self._processArgs = processArgs
+                self.__processArgs = processArgs
             else:
                 raise ValueError('processLineParam has to be a dictionary')
 
-        self._processKWArgs = {}
+        self.__processKWArgs = {}
 
         if processKWArgs is not None:
             if isinstance(processKWArgs, dict):
-                self._processKWArgs = processKWArgs
+                self.__processKWArgs = processKWArgs
             else:
                 raise ValueError('processLineParam has to be a dictionary')
 
-        self._regEx = None
+        self.__regEx = None
         if regexsearch is not None:
             if isinstance(regexsearch, list):
-                self._regEx = []
+                self.__regEx = []
                 for regex in regexsearch:
-                    self._regEx.append(re.compile(regex))
+                    self.__regEx.append(re.compile(regex))
             else:
-                self._regEx = re.compile(regexsearch)
+                self.__regEx = re.compile(regexsearch)
 
-        self._error = ""
-        self._output = []
-        self._rc = None
-        self._regexmatch = None
+        self.__error = ""
+        self.__output = []
+        self.__returnCode = None
+        self.__regexmatch = None
 
     def __bool__(self):
-        if self._command:
+        if self.__command:
             return True
         return False
 
     @property
     def command(self):
         """command to execute"""
-        return self._command
+        return self.__command
 
     @command.setter
     def command(self, value):
         """return current command set in class"""
+        self.__command = None
         self._reset(value)
 
     @property
     def shlexCommand(self):
         """command to submit to subproccess PIPE"""
-        return shlex.split(self._command)
+        return shlex.split(self.__command)
 
     @property
     def error(self):
         """error if command can not be executed"""
-        return self._error
+        return self.__error
 
     @property
     def output(self):
         """captured output"""
-        return self._output
+        return self.__output
 
     @property
     def parsedcommand(self):
@@ -112,17 +115,17 @@ class RunCommand:
         command parsed by shlex
         can be used for debugging
         """
-        return shlex.split(self._command)
+        return shlex.split(self.__command)
 
     @property
     def rc(self):
         """return code"""
-        return self._rc
+        return self.__returnCode
 
     @property
     def regexmatch(self):
         """results of regular expresion search"""
-        return self._regexmatch
+        return self.__regexmatch
 
     def run(self):
         """method to call to execute command"""
@@ -130,67 +133,67 @@ class RunCommand:
 
         self._getCommandOutput()
 
-        if self._output:
+        if self.__output:
             return True
 
         return False
 
     def _reset(self, command=None):
 
-        self._output = []
-        self._error = ""
-        self._regexmatch = None
+        self.__output = []
+        self.__error = ""
+        self.__regexmatch = None
         if command is not None:
-            self._command = command
+            self.__command = command
 
     def _regexMatch(self, line):
         """Have to set the size of in case of list"""
 
         m = None
 
-        if isinstance(self._regEx, list):
+        if isinstance(self.__regEx, list):
 
-            for index, regex in enumerate(self._regEx):
+            for index, regex in enumerate(self.__regEx):
                 m = regex.search(line)
 
                 if m is not None:
-                    if self._regexmatch is None:
-                        self._regexmatch = [None] * len(self._regEx)
+                    if self.__regexmatch is None:
+                        self.__regexmatch = [None] * len(self.__regEx)
 
                     tmpList = []
                     for i in m.groups():
                         tmpList.append(i)
 
-                    self._regexmatch[index] = tmpList
+                    self.__regexmatch[index] = tmpList
 
         else:
 
-            if self._regEx:
-                m = self._regEx.search(line)
+            if self.__regEx:
+                m = self.__regEx.search(line)
 
             if m is not None:
-                if self._regexmatch is None:
-                    self._regexmatch = []
+                if self.__regexmatch is None:
+                    self.__regexmatch = []
 
                 for i in m.groups():
-                    self._regexmatch.append(i)
+                    self.__regexmatch.append(i)
 
     def _getCommandOutput(self):
         """Execute command in a subprocess"""
 
-        self._rc = 10000
+        self.__returnCode = 10000
         rc = 1000
-        if self._commandShlex:
-            cmd = self._command
+        if self.__commandShlex:
+            cmd = self.__command
         else:
-            cmd = shlex.split(self._command)
+            cmd = shlex.split(self.__command)
 
         try:
 
             with subprocess.Popen(
                     cmd, stdout=subprocess.PIPE,
                     bufsize=1,
-                    universal_newlines=self._universalNewLines,
+                    universal_newlines=self.__universalNewLines,
                     stderr=subprocess.STDOUT
                 ) as p:
 
@@ -198,72 +201,72 @@ class RunCommand:
 
                     for l in p.stdout:
 
-                        if self._universalNewLines:
+                        if self.__universalNewLines:
                             line = l
                         else:
                             line = l.decode('utf-8')
 
-                        self._output.append(line)
-                        self._regexMatch(line)
+                        self.__output.append(line)
+                        self.__regexmatch(line)
 
-                        if self._process is not None:
-                            self._process(
+                        if self.__process is not None:
+                            self.__process(
                                 line,
-                                *self._processArgs,
-                                **self._processKWArgs
+                                *self.__processArgs,
+                                **self.__processKWArgs
                             )
 
                 except UnicodeDecodeError as error:
 
                     trb = traceback.format_exc()
                     msg = "Error: {}".format(error.reason)
-                    self._output.append(str(cmd) + '\n')
-                    self._output.append(msg)
-                    self._output.append(trb)
+                    self.__output.append(str(cmd) + '\n')
+                    self.__output.append(msg)
+                    self.__output.append(trb)
 
-                    if self._process is not None:
-                        self._process(
-                                line,
-                                *self._processArgs,
-                                **self._processKWArgs
+                    if self.__process is not None:
+                        self.__process(
+                            line,
+                            *self.__processArgs,
+                            **self.__processKWArgs
                         )
 
                 except KeyboardInterrupt as error:
 
                     trb = traceback.format_exc()
                     msg = "Error: {}".format(error.args)
-                    self._output.append(str(cmd) + '\n')
-                    self._output.append(msg)
-                    self._output.append(trb)
+                    self.__output.append(str(cmd) + '\n')
+                    self.__output.append(msg)
+                    self.__output.append(trb)
 
-                    if self._process is not None:
-                        self._process(
-                                line,
-                                *self._processArgs,
-                                **self._processKWArgs
-                            )
+                    if self.__process is not None:
+                        self.__process(
+                            line,
+                            *self.__processArgs,
+                            **self.__processKWArgs
+                        )
 
                     raise SystemExit(0)
 
                 rcResult = p.poll()
                 if rcResult is not None:
-                    self._rc = rcResult
+                    self.__returnCode = rcResult
                     rc = rcResult
 
         except FileNotFoundError as e:
-            self._error = e
+            self.__error = e
 
         return rc
 
     def _getCommandOutputBackup(self):
         """Execute command in a subprocess"""
 
-        self._rc = 10000
+        self.__returnCode = 10000
         rc = 1000
-        if self._commandShlex:
-            cmd = self._command
+        if self.__commandShlex:
+            cmd = self.__command
         else:
-            cmd = shlex.split(self._command)
+            cmd = shlex.split(self.__command)
 
         try:
 
@@ -278,46 +281,46 @@ class RunCommand:
 
                         line = l.decode('utf-8')
 
-                        self._output.append(line)
-                        self._regexMatch(line)
+                        self.__output.append(line)
+                        self.__regexmatch(line)
 
-                        if self._process is not None:
-                            self._process(
+                        if self.__process is not None:
+                            self.__process(
                                 line,
-                                *self._processArgs,
-                                **self._processKWArgs
+                                *self.__processArgs,
+                                **self.__processKWArgs
                             )
 
                 except UnicodeDecodeError as error:
 
                     trb = traceback.format_exc()
                     msg = "Error: {}".format(error.reason)
-                    self._output.append(str(cmd) + '\n')
-                    self._output.append(msg)
-                    self._output.append(trb)
+                    self.__output.append(str(cmd) + '\n')
+                    self.__output.append(msg)
+                    self.__output.append(trb)
 
-                    if self._process is not None:
-                        self._process(line)
+                    if self.__process is not None:
+                        self.__process(line)
 
                 except KeyboardInterrupt as error:
 
                     trb = traceback.format_exc()
                     msg = "Error: {}".format(error.args)
-                    self._output.append(str(cmd) + '\n')
-                    self._output.append(msg)
-                    self._output.append(trb)
+                    self.__output.append(str(cmd) + '\n')
+                    self.__output.append(msg)
+                    self.__output.append(trb)
 
-                    if self._process is not None:
-                        self._process(line)
+                    if self.__process is not None:
+                        self.__process(line)
 
                     raise SystemExit(0)
 
                 rcResult = p.poll()
                 if rcResult is not None:
-                    self._rc = rcResult
+                    self.__returnCode = rcResult
                     rc = rcResult
 
         except FileNotFoundError as e:
-            self._error = e
+            self.__error = e
 
         return rc
