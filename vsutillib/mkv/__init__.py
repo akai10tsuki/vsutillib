112f92c6fa5f59029176a4686fa0a03048ab43b5
Efrain Vergara <akai10tsuki@gmail.com>
2019-05-23 22:21:57 -0400

    chg: rewrite MKVCommand to be more easily modified some refactoring 	modified:   vsutillib/mkv/__init__.py 	modified:   vsutillib/mkv/classes/__init__.py 	modified:   vsutillib/mkv/classes/mkvcommand.py 	new file:   vsutillib/mkv/classes/verifycommand.py 	modified:   vsutillib/mkv/mkvUtils.py

--- vsutillib/mkv/__init__.py
+++ vsutillib/mkv/__init__.py
@@ -1,4 +1,4 @@
 """VS module names"""
 
-from .classes import MKVCommand, VerifyStructure
+from .classes import MKVCommand, VerifyStructure, VerifyMKVCommand
 from .mkvutils import getMKVMerge, getMKVMergeVersion
