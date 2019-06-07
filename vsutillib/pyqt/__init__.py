57f9b55b2680c2b8abb4390331b705c76a86190d
Efrain Vergara <akai10tsuki@gmail.com>
2019-06-07 17:15:47 -0400

    add: FileListWidget sub class QTextEdit accepts drop files and folders      for file renaming 	modified:   vsutillib/pyqt/__init__.py 	new file:   vsutillib/pyqt/classes/FileListWidget.py 	modified:   vsutillib/pyqt/classes/__init__.py

--- vsutillib/pyqt/__init__.py
+++ vsutillib/pyqt/__init__.py
@@ -4,4 +4,4 @@ PySide2 related classes and functions
 
 # classes
 
-from .classes import OutputTextWidget, QActionWidget, QMenuWidget
+from .classes import FileListWidget, OutputTextWidget, QActionWidget, QMenuWidget
--- vsutillib/pyqt/classes/__init__.py
+++ vsutillib/pyqt/classes/__init__.py
@@ -5,3 +5,4 @@ PySide2 related classes
 from .OutputTextWidget import OutputTextWidget
 from .QActionWidget import QActionWidget
 from .QMenuWidget import QMenuWidget
+from .FileListWidget import FileListWidget
