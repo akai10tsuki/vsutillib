138b2e81b361ecd92d7a92d34ce8eecb14b8ca72
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-12 00:39:47 -0500

    Support Python 3.8 only bump library version to 1.5.0 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -87,9 +87,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
 
         # Implementation
         "Programming Language :: Python :: Implementation :: CPython",
