0bcddf6eee4b89cf5788414f0d2611b8d991c14f
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-25 03:49:40 -0500

    add: PySide2 threads classes to pyqt packages 	modified:   vsutillib/vsutillib-pyqt/vsutillib/pyqt/__init__.py 	new file:   vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/GenericThreads.py 	new file:   vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/QthThreads.py 	modified:   vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/__init__.py

--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/__init__.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/__init__.py
@@ -4,6 +4,15 @@ PySide2 related classes and functions
 
 # classes
 
-from .classes import FileListWidget, OutputTextWidget, QActionWidget, QMenuWidget
+from .classes import (
+    FileListWidget,
+    OutputTextWidget,
+    QActionWidget,
+    QMenuWidget,
+    QthThreadWorker,
+    QthThread,
+    WorkerSignals,
+    Worker,
+)
 from .messagebox import messageBoxYesNo
 from .qtUtils import centerWidgets
--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/__init__.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/__init__.py
@@ -6,3 +6,5 @@ from .OutputTextWidget import OutputTextWidget
 from .QActionWidget import QActionWidget
 from .QMenuWidget import QMenuWidget
 from .FileListWidget import FileListWidget
+from .QthThreads import Worker, WorkerSignals
+from .GenericThreads import QthThread, QthThreadWorker
