15cd8e849f20013339f7b782508dbfd1361981f0
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-26 04:34:16 -0500

    chg: update module impots

--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/__init__.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/__init__.py
@@ -5,14 +5,16 @@ PySide2 related classes and functions
 # classes
 
 from .classes import (
+    DualProgressBar,
     FileListWidget,
     OutputTextWidget,
     QActionWidget,
     QMenuWidget,
+    QPushButtonWidget,
     QthThreadWorker,
     QthThread,
     WorkerSignals,
     Worker,
 )
 from .messagebox import messageBoxYesNo
-from .qtUtils import centerWidgets
+from .qtUtils import centerWidgets, pushButton
--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/__init__.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/__init__.py
@@ -2,9 +2,11 @@
 PySide2 related classes
 """
 
+from .dualProgressBar import DualProgressBar
+from .GenericThreads import QthThread, QthThreadWorker
+from .FileListWidget import FileListWidget
 from .OutputTextWidget import OutputTextWidget
 from .QActionWidget import QActionWidget
 from .QMenuWidget import QMenuWidget
-from .FileListWidget import FileListWidget
+from .QPushButtonWidget import QPushButtonWidget
 from .QthThreads import Worker, WorkerSignals
-from .GenericThreads import QthThread, QthThreadWorker
