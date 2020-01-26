c9305986bd95cf28e9dcc4d4a215d3eced4bfa0a
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-26 04:34:37 -0500

    add: pushButton function

--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/qtUtils.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/qtUtils.py
@@ -7,7 +7,7 @@ utility functions that use PySide2
 
 import logging
 
-from PySide2.QtWidgets import QDesktopWidget
+from PySide2.QtWidgets import QDesktopWidget, QPushButton
 
 
 MODULELOG = logging.getLogger(__name__)
@@ -25,3 +25,23 @@ def centerWidgets(widget, parent=None):
 
     else:
         widget.move(QDesktopWidget().availableGeometry().center() - widget.frameGeometry().center())
+
+def pushButton(label, function, tooltip):
+    """
+    pushButton convenience function for QPushButton definitions
+
+    Args:
+        label (str): label for push button
+        function (func): function to execute when clicked
+        tooltip (str): tool tip for push button
+
+    Returns:
+        QPushButton: PySide2 QPushButton
+    """
+
+    button = QPushButton(label)
+    button.resize(button.sizeHint())
+    button.clicked.connect(function)
+    button.setToolTip(tooltip)
+
+    return button
