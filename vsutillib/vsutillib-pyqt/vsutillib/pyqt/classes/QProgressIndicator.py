7919a43867d85129dbe38638169fcef0a8bbf033
Efrain Vergara <akai10tsuki@gmail.com>
2020-02-04 00:15:46 -0500

    chg: code refactoring add: test function 	modified:   vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/QProgressIndicator.py

--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/QProgressIndicator.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/classes/QProgressIndicator.py
@@ -1,4 +1,28 @@
 """
+
+The MIT License (MIT)
+
+Copyright (c) 2011 Morgan Leborgne
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
 QProgressIndicator
 
 Returns:
@@ -7,7 +31,7 @@ Returns:
 
 import sys
 
-from PySide2.QtCore import Qt, QSize, Property
+from PySide2.QtCore import Qt, QSize
 from PySide2.QtGui import QPainter, QColor
 from PySide2.QtWidgets import QWidget, QApplication, QSizePolicy
 
@@ -16,20 +40,18 @@ class QProgressIndicator(QWidget):
     """
     QProgressIndicator show progress animation
     """
+
     def __init__(self, parent):
         # Call parent class constructor first
         super(QProgressIndicator, self).__init__(parent)
 
-        # Initialize Qt Properties
-        self.setProperties()
-
         # Initialize instance variables
         self.angle = 0
         self.timerId = -1
-        self.delay = 40
-        self.displayedWhenStopped = False
-        self.color = Qt.black
-        self.displayedWhenStopped = None
+
+        self.__delay = 40
+        self.__displayedWhenStopped = False
+        self.__color = Qt.black
 
         # Set size and focus policy
         self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
@@ -38,20 +60,83 @@ class QProgressIndicator(QWidget):
         # Show the widget
         self.show()
 
-    def animationDelay(self):
-        return self.delay
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, value):
+        self.__color = value
+        self.update()
+
+    @property
+    def delay(self):
+        return self.__delay
+
+    @delay.setter
+    def delay(self, value):
+        self.__delay = value
+        if self.timerId != -1:
+            self.killTimer(self.timerId)
+            self.timerId = self.startTimer(self.__delay)
+
+    @property
+    def displayedWhenStopped(self):
+        return self.__displayedWhenStopped
+
+    @displayedWhenStopped.setter
+    def displayedWhenStopped(self, value):
+        self.__displayedWhenStopped = value
+        self.update()
+
+    def heightForWidth(self, w):
+        return w
+
+    def sizeHint(self):
+        return QSize(30, 30)
+
+    def timerEvent(self, event):  # pylint: disable=unused-argument
+        self.angle = (self.angle + 30) % 360
+        self.update()
+
+    def paintEvent(self, event):  # pylint: disable=unused-argument
+        if (not self.displayedWhenStopped) and (not self.isAnimated()):
+            return
+
+        width = min(self.width(), self.height())
+
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+
+        outerRadius = (width - 1) * 0.5
+        innerRadius = (width - 1) * 0.5 * 0.38
+
+        capsuleHeight = outerRadius - innerRadius
+        capsuleWidth = capsuleHeight * 0.23 if (width > 32) else capsuleHeight * 0.35
+        capsuleRadius = capsuleWidth / 2
+
+        for i in range(0, 12):
+            color = QColor(self.color)
+
+            color.setAlphaF(1.0 - (i / 12.0))
+
+            p.setPen(Qt.NoPen)
+            p.setBrush(color)
+            p.save()
+            p.translate(self.rect().center())
+            p.rotate(self.angle - (i * 30.0))
+            p.drawRoundedRect(
+                - capsuleWidth * 0.5,
+                - (innerRadius + capsuleHeight),
+                capsuleWidth,
+                capsuleHeight,
+                capsuleRadius,
+                capsuleRadius,
+            )
+            p.restore()
 
     def isAnimated(self):
         return self.timerId != -1
-
-    def isDisplayedWhenStopped(self):
-        return self.displayedWhenStopped
-
-    def getColor(self):
-        return self.color
-
-    def sizeHint(self):
-        return QSize(20, 20)
 
     def startAnimation(self):
         self.angle = 0
@@ -66,83 +151,46 @@ class QProgressIndicator(QWidget):
         self.timerId = -1
         self.update()
 
-    def setAnimationDelay(self, delay):
-        if self.timerId != -1:
-            self.killTimer(self.timerId)
-
-        self.delay = delay
-
-        if self.timerId != -1:
-            self.timerId = self.startTimer(self.delay)
-
-    def setDisplayedWhenStopped(self, state):
-        self.displayedWhenStopped = state
-        self.update()
-
-    def setColor(self, color):
-        self.color = color
-        self.update()
-
-    def timerEvent(self, event):
-        self.angle = (self.angle + 30) % 360
-        self.update()
-
-    def paintEvent(self, event):
-        if (not self.displayedWhenStopped) and (not self.isAnimated()):
-            return
-
-        width = min(self.width(), self.height())
-
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        outerRadius = (width - 1) * 0.5
-        innerRadius = (width - 1) * 0.5 * 0.38
-
-        capsuleHeight = outerRadius - innerRadius
-        capsuleWidth = capsuleHeight * 0.23 if (width > 32) else capsuleHeight * 0.35
-        capsuleRadius = capsuleWidth / 2
-
-        for i in range(0, 12):
-            color = QColor(self.color)
-
-            if self.isAnimated():
-                color.setAlphaF(1.0 - (i / 12.0))
-            else:
-                color.setAlphaF(0.2)
-
-            painter.setPen(Qt.NoPen)
-            painter.setBrush(color)
-            painter.save()
-            painter.translate(self.rect().center())
-            painter.rotate(self.angle - (i * 30.0))
-            painter.drawRoundedRect(
-                capsuleWidth * -0.5,
-                (innerRadius + capsuleHeight) * -1,
-                capsuleWidth,
-                capsuleHeight,
-                capsuleRadius,
-                capsuleRadius,
-            )
-            painter.restore()
-
-    def setProperties(self):
-        self.delay = Property(int, self.animationDelay, self.setAnimationDelay)
-        self.displayedWhenStopped = Property(
-            bool, self.isDisplayedWhenStopped, self.setDisplayedWhenStopped
-        )
-        self.color = Property(QColor, self.getColor, self.setColor)
-
-
-def TestProgressIndicator():
-    app = QApplication(sys.argv)
-    progress = QProgressIndicator(None)
-    progress.setAnimationDelay(70)
-    progress.startAnimation()
-
-    # Execute the application
-    sys.exit(app.exec_())
-
 
 if __name__ == "__main__":
-    TestProgressIndicator()
+
+
+    from PySide2.QtWidgets import QGridLayout, QMainWindow, QPushButton
+
+    class MainWindow(QMainWindow):
+        """Test the progress bars"""
+
+        def __init__(self, *args, **kwargs):
+            super(MainWindow, self).__init__(*args, **kwargs)
+
+            l = QGridLayout()
+
+            self.pi = QProgressIndicator(self)
+            self.pi.displayedWhenStopped = True
+
+            a = QPushButton("Animate")
+            a.pressed.connect(self.animate)
+            s = QPushButton("Stop Animation")
+            s.pressed.connect(self.stopAnimation)
+
+            l.addWidget(self.pi, 0, 0, Qt.AlignCenter)
+            l.addWidget(a, 1, 0)
+            l.addWidget(s, 2, 0)
+
+            w = QWidget()
+            w.setLayout(l)
+
+            self.setCentralWidget(w)
+
+            self.show()
+
+        def animate(self):
+            self.pi.startAnimation()
+
+        def stopAnimation(self):
+            self.pi.stopAnimation()
+
+    # pylint: disable=invalid-name
+    app = QApplication([])
+    window = MainWindow()
+    sys.exit(app.exec_())
