46cb27b9455daeeeb91dda259db48c9a79b6cf83
Efrain Vergara <akai10tsuki@gmail.com>
2020-02-06 21:04:23 -0500

    chg: remove checkColor move helper

--- vsutillib/vsutillib-pyqt/vsutillib/pyqt/qtUtils.py
+++ vsutillib/vsutillib-pyqt/vsutillib/pyqt/qtUtils.py
@@ -98,24 +98,3 @@ def runFunctionInThread(function, *args, **kwargs):
 
     # Execute
     worker.run()
-
-
-def checkColor(color):
-
-    if color is None:
-
-        if isMacDarkMode() or (platform.system() == "Windows"):
-            color = Qt.white
-        else:
-            color = Qt.black
-
-    #elif isMacDarkMode() or (platform.system() == "Windows"):
-
-    #    if color == Qt.red:
-    #        color = Qt.magenta
-    #    elif color == Qt.darkGreen:
-    #        color = Qt.green
-    #    elif color == Qt.blue:
-    #        color = Qt.cyan
-
-    return color
