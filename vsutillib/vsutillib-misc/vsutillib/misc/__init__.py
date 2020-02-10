7c0a642b9db3d8e0bd3e0ec7b3fd611d251c49ac
Efrain Vergara <akai10tsuki@gmail.com>
2020-02-09 19:13:53 -0500

    add: multipleReplace dict base re.sub

--- vsutillib/vsutillib-misc/vsutillib/misc/__init__.py
+++ vsutillib/vsutillib-misc/vsutillib/misc/__init__.py
@@ -6,3 +6,4 @@ from .classes import XLate
 
 from .cipher import encrypt, decrypt
 from .decorators import staticVars, callCounter
+from .regexutils import multipleReplace
--- vsutillib/vsutillib-misc/vsutillib/misc/regexutils.py
+++ vsutillib/vsutillib-misc/vsutillib/misc/regexutils.py
@@ -5,32 +5,9 @@ Regular Expresions utils
 import re
 
 
-def multiple_replace(adict, text):
+def multipleReplace(aDict, text):
     # Create a regular expression from all of the dictionary keys
-    regex = re.compile("|".join(map(re.escape, adict.keys())))
+    regex = re.compile("|".join(map(re.escape, aDict.keys())))
 
     # For each match, look up the corresponding value in the dictionary
-    return regex.sub(lambda match: adict[match.group(0)], text)
-
-
-# def multipleReplace(d, text):
-# Create a regular expression  from the dictionary keys
-#    regex = re.compile("(%s)" % "|".join(map(re.escape, d.keys())))
-
-# For each match, look-up corresponding value in dictionary
-#    return regex.sub(lambda mo: d[mo.string[mo.start() : mo.end()]], text)
-
-
-class Xlator(dict):
-    """ All-in-one multiple-string-substitution class """
-    def _makeRegex(self):
-        """ Build re object based on the keys of the current dictionary """
-        return re.compile("|".join(map(re.escape, self.keys())))
-
-    def __call__(self, match):
-        """ Handler invoked for each regex match """
-        return self[match.group(0)]
-
-    def xlat(self, text):
-        """ Translate text, returns the modified text. """
-        return self._makeRegex().sub(self, text)
+    return regex.sub(lambda match: aDict[match.group(0)], text)
