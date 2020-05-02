24dc8f5dbc0f98180cdf16c6a6f4dfdcfe6ead2c
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-02 12:15:27 -0400

    bump version to 1.5.0 prepare to publish

--- vsutillib/vsutillib-vsxml/setup.py
+++ vsutillib/vsutillib-vsxml/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 from vsutillib import config
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 PACKAGE = "vsxml"
@@ -22,9 +22,9 @@ def removeTmpDirs():
     """
     delete build directory setup was including files from other builds
     """
-    p = Path('.')
-    eggDirs = [x for x in p.glob('*.egg-info') if x.is_dir()]
-    eggDirs.append(Path('build'))
+    p = Path(".")
+    eggDirs = [x for x in p.glob("*.egg-info") if x.is_dir()]
+    eggDirs.append(Path("build"))
 
     for d in eggDirs:
         if d.is_dir():
@@ -35,23 +35,23 @@ def readme():
     """get README.rst"""
 
     try:
-        with io.open(os.path.join(ROOT, 'README.rst'), encoding='utf-8') as f:
-            long_description = '\n' + f.read()
+        with io.open(os.path.join(ROOT, "README.rst"), encoding="utf-8") as f:
+            long_description = "\n" + f.read()
     except FileNotFoundError:
         long_description = "vsutillib." + PACKAGE + " sub package part of vsutillib"
     return long_description
 
 
 setup(
-    name=config.NAME + '-' + PACKAGE,
-    version='1.0.2',
+    name=config.NAME + "-" + PACKAGE,
+    version="1.5.0",
     description="vsutillib." + PACKAGE + " sub package part of vsutillib",
     long_description=readme(),
     author=config.AUTHOR,
     author_email=config.EMAIL,
-    license='MIT',
-    packages=['vsutillib.' + PACKAGE, 'vsutillib.' + PACKAGE + '.classes'],
-    install_requires=['lxml'],
+    license="MIT",
+    packages=["vsutillib." + PACKAGE, "vsutillib." + PACKAGE + ".classes"],
+    install_requires=["lxml"],
     zip_safe=False,
 )
 
