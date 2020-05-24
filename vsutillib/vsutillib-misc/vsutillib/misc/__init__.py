c0703aaa805d3effdf8850d16c01034c54a822d9
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-24 05:42:17 -0400

    add: strFormatTimeDelta formatter timedelta

--- vsutillib/vsutillib-misc/vsutillib/misc/__init__.py
+++ vsutillib/vsutillib-misc/vsutillib/misc/__init__.py
@@ -7,3 +7,4 @@ from .classes import XLate
 from .cipher import encrypt, decrypt
 from .decorators import staticVars, callCounter
 from .regexutils import multipleReplace
+from .strFormatTimeDelta import strFormatTimeDelta
