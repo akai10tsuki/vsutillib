f3ea571479275c94dcaa917dde835ee9e1a69f55
Efrain Vergara <akai10tsuki@gmail.com>
2020-09-09 12:30:20 -0400

    add: TracksOrder

--- vsutillib/vsutillib-mkv/vsutillib/mkv/classes/__init__.py
+++ vsutillib/vsutillib-mkv/vsutillib/mkv/classes/__init__.py
@@ -6,5 +6,6 @@ from .SourceFiles import SourceFile, SourceFiles
 from .mkvcommand import MKVCommand
 from .MKVCommandParser import MKVCommandParser, MKVParseKey, generateCommand
 from .TrackOptions import TrackOptions
+from .TracksOrder import TracksOrder
 from .verifystructure import VerifyStructure
 from .verifycommand import VerifyMKVCommand
