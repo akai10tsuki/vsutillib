23fa8dafa3369877eef6a72c4ec86d90e4ad9cb1
akai10tuki <akai10tsuki@gmail.com>
2022-01-31 01:20:52 -0500

    Cleanup

--- vsutillib/vsutillib-pyside2/vsutillib/pyside2/qtUtils.py
+++ vsutillib/vsutillib-pyside2/vsutillib/pyside2/qtUtils.py
@@ -42,6 +42,43 @@ def centerWidget(widget: QWidget, parent: Optional[QWidget] = None) -> None:
             - widget.frameGeometry().center()
         )
 
+def checkColor(color: QColor, isDarkMode: bool = False) -> QColor:
+    """
+    checkColor change color according to dark or light mode
+
+    Args:
+        color (SvgColor): color code
+        isDarkMode (bool, optional): True if using dark mode on Windows. Defaults to False.
+
+    Returns:
+        [type]: [description]
+    """
+
+    if color is None:
+
+        if isMacDarkMode() or isDarkMode:
+            color = SvgColor.white
+        else:
+            color = SvgColor.black
+
+    elif isMacDarkMode():
+
+        if color == SvgColor.red:
+            color = SvgColor.magenta
+        elif color == SvgColor.darkgreen:
+            color = SvgColor.green
+        elif color == SvgColor.blue:
+            color = SvgColor.cyan
+
+    elif not isDarkMode:
+
+        if color == SvgColor.cyan:
+            color = SvgColor.dodgerblue
+        elif color == QColor(42, 130, 218):
+            color = SvgColor.dodgerblue
+
+    return color
+
 
 def pushButton(label: str, function: Callable[..., None], tooltip: str) -> QPushButton:
     """
