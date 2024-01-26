6462a9c5ebf14498c53690e9eed389766e185274
akai10tuki <akai10tsuki@gmail.com>
2024-01-26 15:31:44 -0500

    recreate links in linux

---
+++ vsutillib/vsliblinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -s vsutillib-files/vsutillib/files/
+ln -s vsutillib-log/vsutillib/log/
+ln -s vsutillib-macos/vsutillib/macos/
+ln -s vsutillib-media/vsutillib/media/
+ln -s vsutillib-misc/vsutillib/misc/
+ln -s vsutillib-mkv/vsutillib/mkv/
+ln -s vsutillib-network/vsutillib/network/
+ln -s vsutillib-process/vsutillib/process/
+ln -s vsutillib-pyside2/vsutillib/pyside2/
+ln -s vsutillib-pyside2/vsutillib/pyside2/
+ln -s vsutillib-pyside6/vsutillib/pyside6/
+ln -s vsutillib-scripts/vsutillib/scripts/
+ln -s vsutillib-sql/vsutillib/sql/
