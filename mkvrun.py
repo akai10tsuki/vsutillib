1159804846e09b65bcbd3da3e8c6babf628c94ea
akai10tuki <akai10tsuki@gmail.com>
2024-02-09 01:16:14 -0500

    Cleanup

--- mkvrun.py
+++ mkvrun.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python
 
 """
-Execute MKVToolnix genarated command
+Execute MKVToolnix generated command
 and apply it to all files in directory
 """
 
-from vsutillib.scripts import mkvrun
+from vsutillib.scripts import mkvRun
 
 def main():
     """main"""
 
-    mkvrun()
+    mkvRun()
 
 if __name__ == "__main__":
     main()
