1c3d2923a7b38a788102233e1b1a65195d1ac7a7
akai10tuki <akai10tsuki@gmail.com>
2024-03-04 17:17:27 -0500

    Reorder initialization

--- vsutillib/vsutillib-pyside6/vsutillib/pyside6/classes/TabWidget.py
+++ vsutillib/vsutillib-pyside6/vsutillib/pyside6/classes/TabWidget.py
@@ -65,22 +65,22 @@ class TabWidget(QTabWidget):
             self.__tabWidgets.extend(tabWidgets)
 
             for tw in tabWidgets:
-                tabIndex = self.addTab(tw[Key.Widget], tw[Key.Title])
+                tabIndex = self.addTab(tw[Key.Widget], tw[Key.TabText])
                 self.setTabToolTip(tabIndex, tw[Key.ToolTip])
                 widget = tw[Key.Widget]
 
+                try:
+                    widget.tabWidget = self
+                except:
+                    MODULELOG.error("[TabWidget] Error initializing tabWidget.")
                 try:
                     widget.tab = tabIndex
                 except:
                     MODULELOG.error("[TabWidget] Error initializing tab.")
                 try:
-                    widget.title = tw[Key.Title]
+                    widget.tabText = tw[Key.TabText]
                 except:
-                    MODULELOG.error("[TabWidget] Error initializing title.")
-                try:
-                    widget.tabWidget = self
-                except:  # pylint: disable=bare-except
-                    MODULELOG.error("[TabWidget] Error initializing tabWidget.")
+                    MODULELOG.error("[TabWidget] Error initializing tabText.")
 
     # endregion Initialization
 
@@ -170,7 +170,7 @@ class TabWidget(QTabWidget):
 class Key:
 
     Widget: ClassVar[int] = 0
-    Title: ClassVar[int] = 1
+    TabText: ClassVar[int] = 1
     ToolTip: ClassVar[int] = 2
 
 
